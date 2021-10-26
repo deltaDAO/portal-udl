9b7fe5418a219d73aff74db8bdc8e44cdde0c8fd
Moritz Kirstein <mkirstein@protonmail.com>
2021-10-26 19:21:03 +0200

    add overpass font import and family

--- src/global/Styles.tsx
+++ src/global/Styles.tsx
@@ -1,7 +1,9 @@
 import React, { ReactElement, ReactNode } from 'react'
 import { ToastContainer } from 'react-toastify'
 
-import '@oceanprotocol/typographies/css/ocean-typo.css'
+import '@fontsource/overpass/400.css'
+import '@fontsource/overpass/700.css'
+import '@fontsource/overpass/900.css'
 import '../global/styles.css'
 
 export default function Styles({
