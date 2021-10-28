009a014e9891118897f634b5bf4a1277afda9b70
Moritz Kirstein <mkirstein@protonmail.com>
2021-10-28 02:46:55 +0200

    adjust write network script

--- scripts/write-networks-metadata.js
+++ scripts/write-networks-metadata.js
@@ -10,10 +10,13 @@ axios(chainDataUrl).then((response) => {
   //Hack in GX network data
   response.data.push({
     name: 'Gaia-X Testnet',
-    chain: 'GX',
+    chain: 'Gaia-X',
     network: 'testnet',
     rpc: ['https://rpc.gaiaxtestnet.oceanprotocol.com'],
-    faucets: [],
+    faucets: [
+      'https://faucet.gaiaxtestnet.oceanprotocol.com/',
+      'https://faucet.gx.gaiaxtestnet.oceanprotocol.com/'
+    ],
     nativeCurrency: {
       name: 'Gaia-X',
       symbol: 'GX',
@@ -22,16 +25,20 @@ axios(chainDataUrl).then((response) => {
     infoURL: 'https://gaia-x.eu',
     shortName: 'Gaia-X',
     chainId: 2021000,
-    networkId: 2021000
+    networkId: 2021000,
+    explorers: [{ url: 'https://blockscout.gaiaxtestnet.oceanprotocol.com/' }]
   })
 
   //Hack in CX network data
   response.data.push({
     name: 'Catena-X Testnet',
-    chain: 'CX',
+    chain: 'Catena-X',
     network: 'testnet',
     rpc: ['https://rpc.catenaxtestnet.oceanprotocol.com'],
-    faucets: [],
+    faucets: [
+      'https://faucet.catenaxtestnet.oceanprotocol.com/',
+      'https://faucet.cx.catenaxtestnet.oceanprotocol.com/'
+    ],
     nativeCurrency: {
       name: 'Catena-X',
       symbol: 'CX',
@@ -40,7 +47,8 @@ axios(chainDataUrl).then((response) => {
     infoURL: 'https://catena-x.net',
     shortName: 'Catena-X',
     chainId: 2021001,
-    networkId: 2021001
+    networkId: 2021001,
+    explorers: [{ url: 'https://blockscout.catenaxtestnet.oceanprotocol.com/' }]
   })
 
   process.stdout.write(JSON.stringify(response.data, null, '  '))
